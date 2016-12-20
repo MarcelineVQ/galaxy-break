2bb5dc429e073a518300bf1b24bc5e0b7856103b
Lokathor <zefria@gmail.com>
2016-12-20 00:11:57 -0700

    doc update

--- lib/Control/Console/Win32.hs
+++ lib/Control/Console/Win32.hs
@@ -17,12 +17,13 @@ C.context C.baseCtx
 C.include "windows.h"
 
 -- | Make the console ready for use in a platform dependent manner.
+consoleInit :: IO Bool
 consoleInit = do
     hSetBuffering stdout NoBuffering
     setImmediateMode
 
-{-| Sets the console mode to give immediate input instead of line input,
-which implicitly also disables echoing. Returns if it was successful or not.
+{-| Sets the console mode to give immediate input instead of line input, which
+implicitly also disables echoing. Returns if it was successful or not.
 -}
 setImmediateMode :: IO Bool
 setImmediateMode = (/= 0) <$> [C.block| unsigned int {
@@ -36,13 +37,15 @@ setImmediateMode = (/= 0) <$> [C.block| unsigned int {
     } |]
 
 {-| Reads console input events until there is an event for an ASCII character
-and then returns the character involved. We have to skip over some events, because
-something like pressing and holding shift and then pressing 'a' is two events: one
-for the shift key press and then a seperate input event for the 'a' key press,
-producing the 'A' character in the end. Similarly, Alt and Ctrl both produce a key
-press event without producing a usable result.
+and then returns the character involved. We have to skip over some events,
+because something like pressing and holding shift and then pressing 'a' is two
+events: one for the shift key press and then a seperate input event for the
+'a' key press, producing the 'A' character in the end. Similarly, Alt and Ctrl
+both produce a key press event without producing a usable result. This call
+blocks until there eventually is an appropriate key press.
 
-If the standard input handle can't be obtained then this will return '\0' instead.
+If the standard input handle can't be obtained then this will return '\0'
+instead.
 -}
 readOneChar :: IO Char
 readOneChar = (toEnum . fromEnum) <$> [C.block| char {
